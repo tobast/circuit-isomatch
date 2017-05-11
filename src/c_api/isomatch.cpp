276e51ee0e0f26b2ff4a14d1416fea16d71f2802
Théophile Bastian <contact@tobast.fr>
2017-05-11 14:12:37 +0200

    C api: implement gate construction functions

---
+++ src/c_api/isomatch.cpp
@@ -0,0 +1,289 @@
+/**
+ * Implement the C api
+ **/
+
+#include "isomatch.h"
+#include "../isomatch.h"
+
+#include <exception>
+#include <type_traits>
+
+/******************/
+/* Internal state */
+/******************/
+
+static isom_rc lastError = ISOM_RC_OK;
+
+/***************************/
+/* Static helper functions */
+/***************************/
+
+class IsomError : public std::exception {
+    public:
+        IsomError(isom_rc error) : _error(error) {}
+        const char* what() const noexcept { return isom_strerror(_error); }
+        inline isom_rc error() const { return _error; }
+
+    private:
+        isom_rc _error;
+};
+
+static isom_rc handleError(const IsomError& err) {
+    lastError = err.error();
+    return lastError;
+}
+
+static CircuitTree* circuitOfHandle(circuit_handle handle) {
+    if(handle == nullptr)
+        throw IsomError(ISOM_RC_NULLPTR);
+
+    CircuitTree* out = static_cast<CircuitTree*>(handle);
+    return out;
+}
+
+template <typename Circ>
+static Circ* circuitOfHandle(circuit_handle handle) {
+    static_assert(std::is_base_of<CircuitTree, Circ>::value,
+            "Template parameter must derive from CircuitTree");
+    CircuitTree* circ = circuitOfHandle(handle);
+    Circ* out = dynamic_cast<Circ*>(circ);
+    if(out == nullptr)
+        throw IsomError(ISOM_RC_DOMAIN);
+    return out;
+}
+
+static ExpressionBase* exprOfHandle(expr_handle handle) {
+    if(handle == nullptr)
+        throw IsomError(ISOM_RC_NULLPTR);
+
+    ExpressionBase* out = static_cast<ExpressionBase*>(handle);
+    return out;
+}
+
+static WireId* wireOfHandle(wire_handle wire, CircuitGroup* context) {
+    if(context == nullptr)
+        throw IsomError(ISOM_RC_NO_PARENT);
+    return context->wireManager()->wire(wire);
+}
+
+/** Sets the given `parent` as the gate's parent */
+static void setParent(circuit_handle parent, CircuitTree* self) {
+    CircuitGroup* par = circuitOfHandle<CircuitGroup>(parent);
+    par->addChild(self);
+}
+
+/**********************/
+/* API implementation */
+/**********************/
+
+isom_rc isom_last_error() {
+    return lastError;
+}
+
+const char* isom_strerror(isom_rc err_code) {
+    switch(err_code) {
+        case ISOM_RC_OK:
+            return "no error";
+        case ISOM_RC_NULLPTR:
+            return "bad handle (null pointer)";
+        case ISOM_RC_DOMAIN:
+            return "bad handle (cannot cast to right pointer type)";
+        case ISOM_RC_NO_PARENT:
+            return "this circuit handle has no parent group (see "
+                "`build_group_add_child`) and is required to have one in this "
+                "context. This applies eg. when you try to access a wire.";
+        case ISOM_RC_ERROR:
+        default:
+            return "generic error, please submit a bug report";
+    }
+}
+
+// === Generic
+
+int freeze_circuit(circuit_handle circuit) {
+    try {
+        circuitOfHandle(circuit)->freeze();
+        return ISOM_RC_OK;
+    } catch(const IsomError& e) {
+        return handleError(e);
+    }
+}
+
+int free_circuit(circuit_handle circuit) {
+    try {
+        delete circuitOfHandle(circuit);
+        return ISOM_RC_OK;
+    } catch(const IsomError& e) {
+        return handleError(e);
+    }
+}
+
+// === Assert
+
+circuit_handle build_assert(circuit_handle parent,
+        const char* name,
+        expr_handle expr)
+{
+    try {
+        CircuitAssert* out = new CircuitAssert(name, exprOfHandle(expr));
+        setParent(parent, out);
+        return out;
+    } catch(const IsomError& e) {
+        handleError(e);
+        return nullptr;
+    }
+}
+
+int build_assert_add_input(circuit_handle self, wire_handle wire) {
+    try {
+        CircuitAssert* asser = circuitOfHandle<CircuitAssert>(self);
+        asser->addInput(wireOfHandle(wire, asser->ancestor()));
+        return ISOM_RC_OK;
+    } catch(const IsomError& e) {
+        return handleError(e);
+    }
+}
+
+// === Comb
+
+circuit_handle build_comb(circuit_handle parent) {
+    try {
+        CircuitComb* out = new CircuitComb();
+        setParent(parent, out);
+        return out;
+    } catch(const IsomError& e) {
+        handleError(e);
+        return nullptr;
+    }
+}
+
+int build_comb_add_input(circuit_handle self, wire_handle wire) {
+    try {
+        CircuitComb* circ = circuitOfHandle<CircuitComb>(self);
+        circ->addInput(wireOfHandle(wire, circ->ancestor()));
+        return ISOM_RC_OK;
+    } catch(const IsomError& e) {
+        return handleError(e);
+    }
+}
+
+int build_comb_add_output(circuit_handle self,
+        wire_handle wire,
+        expr_handle expr)
+{
+    try {
+        CircuitComb* circ = circuitOfHandle<CircuitComb>(self);
+        circ->addOutput(exprOfHandle(expr),
+                wireOfHandle(wire, circ->ancestor()));
+        return ISOM_RC_OK;
+    } catch(const IsomError& e) {
+        return handleError(e);
+    }
+}
+
+// === Delay
+
+circuit_handle build_delay(circuit_handle parent,
+        wire_handle input,
+        wire_handle output)
+{
+    try {
+        CircuitGroup* par = circuitOfHandle<CircuitGroup>(parent);
+        CircuitDelay* delay = new CircuitDelay(
+                wireOfHandle(input, par),
+                wireOfHandle(output, par));
+        setParent(parent, delay);
+        return delay;
+    } catch(const IsomError& e) {
+        handleError(e);
+        return nullptr;
+    }
+}
+
+// === Group
+
+circuit_handle build_group(const char* name) {
+    try {
+        CircuitGroup* out = new CircuitGroup(name);
+        return out;
+    } catch(const IsomError& e) {
+        handleError(e);
+        return nullptr;
+    }
+}
+
+int build_group_add_child(circuit_handle self, circuit_handle child) {
+    try {
+        CircuitGroup* group = circuitOfHandle<CircuitGroup>(self);
+        group->addChild(circuitOfHandle(child));
+        return ISOM_RC_OK;
+    } catch(const IsomError& e) {
+        return handleError(e);
+    }
+}
+
+int build_group_add_input(circuit_handle self,
+        wire_handle actual,
+        wire_handle formal)
+{
+    try {
+        CircuitGroup* group = circuitOfHandle<CircuitGroup>(self);
+        if(group->ancestor() != nullptr) {
+            group->addInput(IOPin(
+                        wireOfHandle(formal, group->ancestor()),
+                        wireOfHandle(actual, group),
+                        group));
+        }
+        else
+            group->addInput(formal, wireOfHandle(actual, group));
+
+        return ISOM_RC_OK;
+    } catch(const IsomError& e) {
+        return handleError(e);
+    }
+}
+
+int build_group_add_output(circuit_handle self,
+        wire_handle actual,
+        wire_handle formal)
+{
+    try {
+        CircuitGroup* group = circuitOfHandle<CircuitGroup>(self);
+        if(group->ancestor() != nullptr) {
+            group->addOutput(IOPin(
+                        wireOfHandle(formal, group->ancestor()),
+                        wireOfHandle(actual, group),
+                        group));
+        }
+        else
+            group->addOutput(formal, wireOfHandle(actual, group));
+
+        return ISOM_RC_OK;
+    } catch(const IsomError& e) {
+        return handleError(e);
+    }
+}
+
+// === Tristate
+
+circuit_handle build_tristate(circuit_handle parent,
+        wire_handle from,
+        wire_handle to,
+        wire_handle enable)
+{
+    try {
+        CircuitGroup* par = circuitOfHandle<CircuitGroup>(parent);
+        CircuitTristate* out = new CircuitTristate(
+                wireOfHandle(from, par),
+                wireOfHandle(to, par),
+                wireOfHandle(enable, par));
+        setParent(parent, out);
+        return out;
+    } catch(const IsomError& e) {
+        handleError(e);
+        return nullptr;
+    }
+}
+
+// === Expressions
+
--- src/c_api/isomatch.h
+++ src/c_api/isomatch.h
@@ -40,6 +40,8 @@ typedef enum isom_rc {
     ISOM_RC_OK      = 0, ///< Everything went fine
     ISOM_RC_NULLPTR = 1, ///< One of the parameters was a null pointer
     ISOM_RC_DOMAIN  = 2, ///< One of the parameters has the wrong pointer type
+    ISOM_RC_NO_PARENT = 3, ///< A circuit outside of a group was used, while
+                           ///< it needed to be in a group
     ISOM_RC_ERROR   = 255, ///< An undefined error occurred
 } isom_rc;
 //TODO complete this list
@@ -51,9 +53,9 @@ typedef enum isom_rc {
 /// Return the error code of the last error that occurred
 isom_rc isom_last_error();
 
-/** Return a human-friendly string describing the last error that occurred.
+/** Return a human-friendly string describing the given error.
  * Note that you don't have to `free` the returned string. */
-const char* isom_strerror();
+const char* isom_strerror(isom_rc err_code);
 
 /*****************************************************************************/
 /* Enumerations                                                              */
@@ -113,67 +115,75 @@ int free_circuit(circuit_handle circuit);
 /****************/
 
 /// Build an assert gate
-circuit_handle build_assert(const char* name, expr_handle expr);
+circuit_handle build_assert(circuit_handle parent,
+        const char* name,
+        expr_handle expr);
 
 /** Add an input wire to a given gate (order matters!)
  * @return 0 on success, > 0 on failure
  */
-int build_assert_add_input(wire_handle wire);
+int build_assert_add_input(circuit_handle self, wire_handle wire);
 
 /**************/
 /* Comb gates */
 /**************/
 
 /// Build a comb gate, which will need to be edited with the functions below
-circuit_handle build_comb();
+circuit_handle build_comb(circuit_handle parent);
 
 /** Add an input wire to a given combinator gate
  * @return 0 on success, > 0 on failure
  */
-int build_comb_add_input(wire_handle wire);
+int build_comb_add_input(circuit_handle self, wire_handle wire);
 
 /** Add an output wire, alongside with an expression, to a given comb gate
  * @return 0 on success, > 0 on failure
  */
-int build_comb_add_output(wire_handle wire, expr_handle expr);
+int build_comb_add_output(circuit_handle self,
+        wire_handle wire,
+        expr_handle expr);
 
 /***************/
 /* Delay gates */
 /***************/
 
 /// Build a delay gate
-circuit_handle build_delay(wire_handle input, wire_handle output);
+circuit_handle build_delay(circuit_handle parent,
+        wire_handle input,
+        wire_handle output);
 
 /*************************/
 /* Hierarchy group gates */
 /*************************/
 
 /** Build a hierarchy group gate, which will need to be edited with the
- * functions below */
+ * functions below.
+ * A group does not necessarily belong to another group, thus there is no
+ * `parent` argument, and you must call `build_group_add_child` yourself. */
 circuit_handle build_group(const char* name);
 
 /** Add the given circuit as a child of the given group.
  * @return 0 on success, > 0 on failure
  */
-int build_group_add_child(circuit_handle child, circuit_handle parent);
+int build_group_add_child(circuit_handle self, circuit_handle child);
 
 /** Add an input pin to the given circuit group.
- * @param group The group to work on
+ * @param self  The group to work on
  * @param actual The name of the pin when accessed from inside the group
  * @param formal The name of the pin when accessed from outside the group
  * @return 0 on success, > 0 on failure
  */
-int build_group_add_input(circuit_handle group,
+int build_group_add_input(circuit_handle self,
         wire_handle actual,
         wire_handle formal);
 
 /** Add an output pin to the given circuit group.
- * @param group The group to work on
+ * @param self The group to work on
  * @param actual The name of the pin when accessed from inside the group
  * @param formal The name of the pin when accessed from outside the group
  * @return 0 on success, > 0 on failure
  */
-int build_group_add_output(circuit_handle group,
+int build_group_add_output(circuit_handle self,
         wire_handle actual,
         wire_handle formal);
 
@@ -182,7 +192,8 @@ int build_group_add_output(circuit_handle group,
 /******************/
 
 /// Build a tristate gate
-circuit_handle build_tristate(wire_handle from,
+circuit_handle build_tristate(circuit_handle parent,
+        wire_handle from,
         wire_handle to,
         wire_handle enable);
 
