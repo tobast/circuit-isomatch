dd160913e577a5f13893de2ac9a3dc22d1a401d4
Théophile Bastian <contact@tobast.fr>
2017-05-26 17:43:10 +0200

    tests: equal: add scrambling on single circuit

--- util/lang/main_equal.cpp
+++ util/lang/main_equal.cpp
@@ -1,17 +1,22 @@
 #include <cstdio>
 #include <iostream>
+#include <scramble.h>
 #include "aux.h"
 using namespace std;
 
-int main(int, char** argv) {
+int main(int argc, char** argv) {
     CircuitGroup* circuit1 = parse(argv[1]);
-    CircuitGroup* circuit2 = parse(argv[2]);
+    CircuitGroup* circuit2 = nullptr;
+    if(argc > 2)
+        circuit2 = parse(argv[2]);
+    else
+        circuit2 = scrambleCircuit(circuit1);
     circuit1->freeze();
     circuit2->freeze();
 
-    cout << circuit1->equals(circuit2) << endl;
-    cout << " =========== " << endl;
-    cout << circuit2->equals(circuit1) << endl;
+    cout << circuit1->equals(circuit2)
+         << circuit2->equals(circuit1)
+         << endl;
 
     delete circuit1;
     delete circuit2;
