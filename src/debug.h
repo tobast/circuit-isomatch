97ed0505cf4520dc7ca7a6171b987a1f5872642c
Théophile Bastian <contact@tobast.fr>
2017-06-06 17:10:31 +0200

    debug: turn off exact match debug messages for now

--- src/debug.h
+++ src/debug.h
@@ -1,7 +1,7 @@
 /** Few debug features. These flags should all be turned off in release. */
 
 #ifdef DEBUG_ALL
-#define DEBUG_EQUAL
+//#define DEBUG_EQUAL
 #endif
 
 /* Debug printing for sub-equality matches */
