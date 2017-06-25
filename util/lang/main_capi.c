54d7f2f5b54954a4f9b9c8dadb14b4ebfe386051
Théophile Bastian <contact@tobast.fr>
2017-06-25 18:46:05 +0200

    tests: add tests for C api subcircuit find

--- util/lang/main_capi.c
+++ util/lang/main_capi.c
@@ -7,7 +7,9 @@ int main() {
     circuit_handle g_root = build_group("root");
     build_group_add_input(g_root, "p1", "p1");
     build_group_add_input(g_root, "p2", "p2");
+    build_group_add_input(g_root, "p3", "p3");
     build_group_add_output(g_root, "out", "out");
+    build_group_add_output(g_root, "mux1out", "mux1out");
 
     circuit_handle g_sub = build_group("sub");
     build_group_add_input(g_sub, "inp", "p1");
@@ -30,11 +32,41 @@ int main() {
             build_expr_unop(UNot, build_expr_var(0)));
     build_tristate(g_root, "p2", "out", "delay_out_not");
 
+    circuit_handle c_mux1_not = build_comb(g_root);
+    build_comb_add_input(c_mux1_not, "p1");
+    build_comb_add_output(c_mux1_not, "np1",
+            build_expr_unop(UNot, build_expr_var(0)));
+    build_tristate(g_root, "p2", "mux1out", "p1");
+    build_tristate(g_root, "p3", "mux1out", "np1");
 
     freeze_circuit(g_root);
     printf("%lX\n", sign(g_root));
 
+    circuit_handle g_needle = build_group("needle_mux");
+    build_group_add_input(g_needle, "a", "a");
+    build_group_add_input(g_needle, "b", "b");
+    build_group_add_input(g_needle, "sel", "sel");
+    build_group_add_output(g_needle, "out", "out");
+    circuit_handle c_needle_not = build_comb(g_needle);
+    build_comb_add_input(c_needle_not, "sel");
+    build_comb_add_output(c_needle_not, "nsel",
+            build_expr_unop(UNot, build_expr_var(0)));
+    build_tristate(g_needle, "a", "out", "sel");
+    build_tristate(g_needle, "b", "out", "nsel");
+
+    freeze_circuit(g_needle);
+
+    match_results* res = subcircuit_find(g_needle, g_root);
+    int matches = 0;
+    while(res != NULL) {
+        res = res->next;
+        ++matches;
+    }
+    printf("%d MUX\n", matches);
+
+    free_match_results(res);
     free_circuit(g_root);
+    free_circuit(g_needle);
 
     return 0;
 }
